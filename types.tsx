c6dc9849df1246cdf4658883ea140001985781d0
ThisIsBond <joshisaurabh7788@gmail.com>
2022-03-15 15:49:18 +0530

    Implemented AWS AppSync database with GraphQL Api and also implemented user Model.

--- types.tsx
+++ types.tsx
@@ -3,6 +3,17 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
+/*
+TypeScript is a typed superset of JavaScript that was designed to solve many of the pain points of writing applications in JavaScript.
+TypeScript compiles down to plain JavaScript and is backwards-compatible with a few edge cases, making it a great target for older environments.
+
+With features like interfaces, generics, optional type-checking, and type inference, as well as the latest features from ES6 like decorators, 
+async/await, and more, TypeScript stands out greatly when it comes to developer productivity.
+
+On the other hand, GraphQL is a query language for APIs that uses its type system to describe data fields, preventing over and under-fetching. 
+Additionally, GraphQL can greatly help in the area of API versioning.
+*/
+
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
